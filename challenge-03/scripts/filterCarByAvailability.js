51b734a0e9b452408a51871bfac0669d13924ae0
Fachrul <fachrulfatahillah@gmail.com>
2022-10-13 19:35:56 +0700

    Add Challenge 3

---
+++ challenge-03/scripts/filterCarByAvailability.js
@@ -0,0 +1,12 @@
+function filterCarByAvailability(cars) {
+  // Sangat dianjurkan untuk console.log semua hal hehe
+  console.log(cars);
+
+  // Tempat penampungan hasil
+  const result = [];
+
+  // Tulis code-mu disini
+  cars.forEach((state) => (state.available ? result.push(state) : ''));
+  // Rubah code ini dengan array hasil filter berdasarkan availablity
+  return result;
+}
